628b3e79298636b004edf111a64bfd3ad3c7adfa
Aronsson Hannes <hannes.aronsson@scania.com>
2023-06-26 11:07:57 +0200

    fixed language server

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,19 +4,40 @@ local on_attach = config.on_attach
 local capabilities = config.capabilities
 local lspconfig = require("lspconfig")
 
--- lspconfig.pyright.setup({
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = (function()
+  local capabilities = require("plugins.configs.lspconfig").capabilities
+  capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+  return capabilities
+  end)(),
+  filetypes = {"python"},
+})
+
+
+lspconfig.ruff_lsp.setup({
+  on_attach = function(client, bufnr)
+  client.server_capabilities.hoverProvider = false
+  require("plugins.configs.lspconfig").on_attach(client, bufnr)
+  end,
+  capabilities = capabilities
+})
+
+-- lspconfig.jedi_language_server.setup({
 --   on_attach = on_attach,
 --   capabilities = capabilities,
 --   filetypes = {"python"},
 -- })
---
-lspconfig.jedi_language_server.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"python"},
-})
 
 -- lspconfig.ruff_lsp.setup({
 --   on_attach = on_attach,
 --   capabilities = capabilities 
 -- })
+
+-- lspconfig.pylsp.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = {"python"},
+-- })
+
